b761228919dc87d221d0c1031702d4d82d9b2f1f
amibhikari2007-spec <amibhikari2007@gmail.com>
2026-03-22 16:18:45 +0530

    Create Booking.js

---
+++ Booking.js
@@ -0,0 +1,30 @@
+const mongoose=require("mongoose");
+
+const bookingSchema=new mongoose.Schema({
+
+customerName:String,
+
+eventType:String,
+
+packageName:String,
+
+totalAmount:Number,
+
+advancePaid:Number,
+
+balanceDue:Number,
+
+eventDate:String,
+
+invoiceNumber:String,
+
+status:String,
+
+createdAt:{
+type:Date,
+default:Date.now
+}
+
+});
+
+module.exports=mongoose.model("Booking",bookingSchema);
