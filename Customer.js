4c6640a7cad195c5215cb8dea5469eeacfb8fc87
amibhikari2007-spec <amibhikari2007@gmail.com>
2026-03-22 15:57:54 +0530

    Create Customer model with schema definition

---
+++ Customer.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema({
+
+name:String,
+
+phone:String,
+
+address:String,
+
+createdAt:{
+type:Date,
+default:Date.now
+}
+
+});
+
+module.exports=mongoose.model("Customer",customerSchema);
