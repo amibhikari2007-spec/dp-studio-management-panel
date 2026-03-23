394d937e43ad6cdbf6a79098870d836ada8e3d6e
amibhikari2007-spec <amibhikari2007@gmail.com>
2026-03-23 23:51:27 +0530

    Update User.js

--- User.js
+++ User.js
@@ -2,25 +2,25 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
 
-username: {
-type: String,
-required: true,
-unique: true
+username:{
+type:String,
+required:true,
+unique:true
 },
 
-password: {
-type: String,
-required: true
+password:{
+type:String,
+required:true
 },
 
-role: {
-type: String,
-default: "staff"
+role:{
+type:String,
+default:"staff"
 },
 
-createdAt: {
-type: Date,
-default: Date.now
+createdAt:{
+type:Date,
+default:Date.now
 }
 
 });
