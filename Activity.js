990a2faf9b8189f52324095c23a45b206067f83e
amibhikari2007-spec <amibhikari2007@gmail.com>
2026-03-25 16:47:02 +0530

    Update Activity.js

--- Activity.js
+++ Activity.js
@@ -2,26 +2,24 @@ const mongoose = require("mongoose");
 
 const activitySchema = new mongoose.Schema({
 
-username: {
-type: String,
-required: true
+username:{
+type:String,
+required:true
 },
 
-action: {
-type: String,
-required: true
+action:{
+type:String,
+required:true
 },
 
-details: {
-type: String,
-default: ""
-},
-
-date: {
-type: Date,
-default: Date.now
+details:{
+type:String,
+required:true
 }
 
+},{
+timestamps:true   // ✅ THIS LINE FIXES INVALID DATE
 });
 
-module.exports = mongoose.model("Activity", activitySchema);
+module.exports =
+mongoose.model("Activity",activitySchema);
