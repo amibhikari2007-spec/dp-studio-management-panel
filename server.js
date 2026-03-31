ff3624beae2635bdda2c15f032b78e5f946d6480
amibhikari2007-spec <amibhikari2007@gmail.com>
2026-03-31 15:03:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use('/api', studioRoutes);
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "public")));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/receipts', express.static(path.join(__dirname, 'receipts')));
 
